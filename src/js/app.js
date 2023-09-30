c7b04c198525397061539800f93745c13eac64f7
suleymanibishov <suleyman22022@gmail.com>
2023-09-30 13:18:26 +0400

    add dorpdawn

--- src/js/app.js
+++ src/js/app.js
@@ -14,3 +14,24 @@ function CanvasToggle(){
     const list = document.querySelector(".offCanvas__wrap");
     list.style.transform = "translateX(100%)";
 }
+let tgmobile_menu = document.querySelector(".tgmobile__menu");
+document.querySelector(".mobile-nav-toggler").addEventListener("click",function(){
+    document.querySelector(".tgmobile__menu").style.transform = "translateX(0)";
+})
+
+document.querySelector(".close-btn").addEventListener("click",function(){
+    document.querySelector(".tgmobile__menu").style.transform = "translateX(101%)";
+})
+
+document.querySelectorAll(".dropdown-btn").forEach(element => {
+    element.addEventListener("click",function(){
+        if(element.parentElement.querySelector(".sub-menu").style.display == "block"){
+           element.parentElement.querySelector(".sub-menu").style.display = "none";
+        }
+        else{
+        element.parentElement.querySelector(".sub-menu").style.display = "block"
+        
+        }
+
+    })
+});
